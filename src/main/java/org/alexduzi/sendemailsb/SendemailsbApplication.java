2deef9e0b8efc79cd8722f00c38fe4edb797aa4c
alexduzi <duzihd@gmail.com>
2025-06-03 14:00:28 -0300

    first commit

---
+++ src/main/java/org/alexduzi/sendemailsb/SendemailsbApplication.java
@@ -0,0 +1,13 @@
+package org.alexduzi.sendemailsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendemailsbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SendemailsbApplication.class, args);
+    }
+
+}
