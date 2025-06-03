357b29b5cee090eb8b40a850144882dc91fd1095
alexduzi <duzihd@gmail.com>
2025-06-03 15:26:51 -0300

    validating reading step

--- src/main/java/org/alexduzi/sendemailsb/step/SendEmailUserStepConfig.java
+++ src/main/java/org/alexduzi/sendemailsb/step/SendEmailUserStepConfig.java
@@ -5,6 +5,7 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,12 +22,12 @@ public class SendEmailUserStepConfig {
 
     @Bean
     public Step sendEmailUserStep(JobRepository jobRepository,
-                                  ItemReader<UserBookLoan> itemReaderUserBookLoan {
+                                  ItemReader<UserBookLoan> readUsersWithLoansCloseToReturnReader,
+                                  ItemWriter<UserBookLoan> sendEmailRequestReturnWriter) {
         return new StepBuilder("sendEmailUserStep", jobRepository)
                 .<UserBookLoan, UserBookLoan>chunk(1, transactionManager)
-                .reader(itemReaderUserBookLoan)
-                .processor(null)
-                .writer(null)
+                .reader(readUsersWithLoansCloseToReturnReader)
+                .writer(sendEmailRequestReturnWriter)
                 .build();
     }
 }
---
+++ src/main/java/org/alexduzi/sendemailsb/writer/SendEmailRequestReturnWriterConfig.java
@@ -0,0 +1,51 @@
+package org.alexduzi.sendemailsb.writer;
+
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import org.alexduzi.sendemailsb.domain.UserBookLoan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SendEmailRequestReturnWriterConfig {
+
+    private static Logger LOG = LoggerFactory.getLogger(SendEmailRequestReturnWriterConfig.class);
+
+    private final SendGrid sendGrid;
+
+    public SendEmailRequestReturnWriterConfig(SendGrid sendGrid) {
+        this.sendGrid = sendGrid;
+    }
+
+    @Bean
+    public ItemWriter<UserBookLoan> sendEmailRequestReturnWriter() {
+//        return items -> items.forEach((item) -> sendEmail(item));
+        return items -> items.forEach(System.out::println);
+    }
+
+    private void sendEmail(Mail email) {
+        LOG.info("Sending mail");
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(email.build());
+            LOG.info("[WRITER STEP] Send email to: " + email.build());
+            Response response = sendGrid.api(request);
+            if (response.getStatusCode() >= 400 && response.getStatusCode() <= 500) {
+                LOG.error("Error sending email: " + response.getBody());
+                throw new Exception(response.getBody());
+            }
+            LOG.info("Email sent! Status = " + response.getStatusCode());
+        } catch (Exception e) {
+            e.getMessage();
+        }
+    }
+
+}
